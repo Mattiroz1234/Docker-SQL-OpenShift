9c86946720e088a390792412261457a9baa5f9b4
matti <m0556764778@gmail.com>
2025-08-14 17:15:55 +0300

    dockerfile

--- data-loader/services/data_loader.py
+++ data-loader/services/data_loader.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine, text
 from pydantic import BaseModel
 
-app = FastAPI(title="FastAPI + MySQL")
+app = FastAPI()
 
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "pwd"
@@ -16,12 +16,12 @@ engine = create_engine(DATABASE_URL)
 
 class User(BaseModel):
     id: int
-    name: str
-    email: str
+    first_name: str
+    last_name: str
 
 @app.get("/users")
 def read_users():
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT id, name, email FROM users"))
-        users = [User(id=row.id, name=row.name, email=row.email) for row in result]
+        result = conn.execute(text("SELECT * FROM data"))
+        users = [User(id=row.id, first_name=row.first_name, last_name=row.last_name) for row in result]
     return users
