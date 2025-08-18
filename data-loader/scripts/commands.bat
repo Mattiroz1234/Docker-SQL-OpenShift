2764159a0a889d29ee2efcb74e1bb300805527a5
matti <m0556764778@gmail.com>
2025-08-18 11:59:20 +0300

    create & insert mysql db

--- data-loader/scripts/commands.bat
+++ data-loader/scripts/commands.bat
@@ -1,4 +1,22 @@
+# bild mysql application
 oc apply -f mysql-secret.yaml
 oc apply -f mysql-pvc.yaml
 oc apply -f mysql-deployment.yaml
 oc apply -f mysql-service.yaml
+
+# bild fastapi application
+docker build -t mattiroz/fastapi-app:latest .
+docker push mattiroz/fastapi-app:latest
+oc apply -f fastapi-deployment.yaml
+oc apply -f fastapi-service.yaml
+oc apply -f fastapi-route.yaml
+
+# create & insert db
+oc cp sql/data_create.sql mysql-8489dbcf89-wwxlr:/tmp/sql.data_create.sql
+oc cp sql/data_insert.sql mysql-8489dbcf89-wwxlr:/tmp/sql.data_insert.sql
+oc exec -it mysql-8489dbcf89-wwxlr -- bash
+mysql -u root -p
+CREATE DATABASE testdb;
+USE testdb;
+source /tmp/sql.data_create.sql;
+source /tmp/sql.data_insert.sql;
---
+++ data-loader/scripts/sql/data_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO data (first_name, last_name) VALUES
+('Vincent', 'Van Gogh'),
+('Leonardo', 'Da Vinci'),
+('Edvard', 'Munch'),
+('Claude', 'Monet'),
+('Pablo', 'Picasso');
